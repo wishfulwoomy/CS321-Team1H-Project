ebb45deaf806a58d6ddfa6460d6557ba38101805
wishfulwoomy <filipookie@gmail.com>
2026-03-23 22:52:24 -0500

    Added a few basics to GameParser.java and GameSearch.java.

--- src/GameParser.java
+++ src/GameParser.java
@@ -1,4 +1,13 @@
-public class GameParser 
+import java.util.ArrayList;
+
+public class GameParser
 {
-    
+    /**
+     * The overall collection of Games, taken from the XML file.
+     */
+    ArrayList<Game> GameCollection = new ArrayList<Game>();
+    /**
+     * The path to the XML file.
+     */
+    String filePath;
 }
---
+++ src/GameSearch.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+// NOTES FOR LATER: user needs to be able to search by query or filter OR BOTH
+// current layout doesn't support both. fix that
+
+public class GameSearch {
+    /**
+     * @param query The text query, taken from user input and used to search GameCollection.
+     * @return A filtered list of games.
+     */
+    public ArrayList<Game> searchByQuery(String query) {
+        ArrayList<Game> SearchResults = new ArrayList<Game>();
+        // TO-DO: IMPLEMENT SEARCH
+        return SearchResults;
+    }
+    public ArrayList<Game> searchByFilter() {
+        ArrayList<Game> SearchResults = new ArrayList<Game>();
+        // TO-DO: IMPLEMENT SEARCH
+        return SearchResults;
+    }
+}
