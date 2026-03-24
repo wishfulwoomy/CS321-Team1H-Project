cba0c6da35f10273c3327d9f78dc5bd500276a37
Eve Wall <emw0163@uah.edu>
2026-03-23 19:20:06 -0500

    Added User class

---
+++ src/User.java
@@ -0,0 +1,64 @@
+public class User {
+    private String name;
+    private int userID;
+    private String password;
+    //ArrayList<Wishlist> AllWishlists
+
+    User(String username, int id, String pass){
+        name = username;
+        userID = id;
+        password = pass;
+        //AllWishlists = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public int getID(){
+        return userID;
+    }
+
+    public void setID(int newID){
+        userID = newID;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String newPass){
+        password = newPass;
+    }
+
+    /*
+    public bool isCorrect(){
+
+    }
+    */
+    /*I'm assuming that this is checking to see if the pass the user types in is the same as the pass the user set.
+    If so, this might be a class that we need to put in  LoginView or something. */
+
+    /*
+    public void createWishlist(){
+        Wishlist list = new Wishlist(<parameters>);
+        AllWishLists.add(list);
+    }
+    */
+
+    /*
+    public ArrayList<Wishlist> getWishlists(){
+        return AllWishLists;
+    }
+    */
+
+    /*
+    public void deleteWishlist(Wishlist listName){
+        AllWishLists.remove(listName)
+    }
+    */
+}
