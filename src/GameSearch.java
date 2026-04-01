997d340c637bbc02316ac5cb7eef32a7b0754b10
wishfulwoomy <filipookie@gmail.com>
2026-04-01 10:16:59 -0500

    Finished text query searching in GameSearch. Added GameSearchTester.

--- src/GameSearch.java
+++ src/GameSearch.java
@@ -1,21 +1,39 @@
 import java.util.ArrayList;
-
-// NOTES FOR LATER: user needs to be able to search by query or filter OR BOTH
-// current layout doesn't support both. fix that
+import java.util.List;
 
 public class GameSearch {
+    private List<Game> SearchResults = new ArrayList<Game>();
+
     /**
+     * Function to search through the full game catalog and return results matching the user's search criteria.
+     * @param games The full list of games to search through. NOTE: will likely be replaced with games list from GameParse.
      * @param query The text query, taken from user input and used to search GameCollection.
      * @return A filtered list of games.
      */
-    public ArrayList<Game> searchByQuery(String query) {
-        ArrayList<Game> SearchResults = new ArrayList<Game>();
-        // TO-DO: IMPLEMENT SEARCH
+    public List<Game> searchGames(ArrayList<Game> games, String query) {
+        SearchResults.clear();
+        for (int i=0; i<games.size(); i++) {
+            if (query != null) {
+                if (!matchQuery(games.get(i), query))
+                    continue;
+            }
+            SearchResults.add(games.get(i)); // if game matches query, add to list
+        }
         return SearchResults;
     }
-    public ArrayList<Game> searchByFilter() {
-        ArrayList<Game> SearchResults = new ArrayList<Game>();
-        // TO-DO: IMPLEMENT SEARCH
-        return SearchResults;
+
+    /**
+     * Function to check if a certain game matches a user-given search query.
+     * @param game The game to check against the query.
+     * @param query A String search query
+     * @return A boolean true/false. True if the game matches the query, false otherwise.
+     */
+    public boolean matchQuery(Game game, String query) {
+        return game.toString().toLowerCase().contains(query.toLowerCase());
+    }
+
+    public boolean matchFilter(Game game) {
+    // NOT FINISHED; still figuring out how to do the enum filter
+    return true; //temp
     }
 }
---
+++ src/GameSearchTester.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameSearchTester {
+    // NOT FINISHED
+    List<Game> gamesList = new ArrayList<>();
+}
