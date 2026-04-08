6c6becf503a4092647c1a0aed73b6f00f10c2a65
Eve Wall <emw0163@uah.edu>
2026-04-08 12:51:30 -0500

    Imported javafxml in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package main.java.org.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
