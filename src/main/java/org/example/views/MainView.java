b2f7d0155907767f2444c73c9ce95325832e775f
Eve Wall <emw0163@uah.edu>
2026-04-08 12:46:20 -0500

    Experimenting in Main
    also commented out MainView.java for testing

--- src/main/java/org/example/views/MainView.java
+++ src/main/java/org/example/views/MainView.java
@@ -3,26 +3,24 @@ package main.java.org.example.views;
 import main.java.org.example.model.Game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class MainView {
-    private String search;
-    private JPanel mainViewPanel;
-    private JButton favoriteButton;
-    private JLabel selectedGame;
-    private JLabel gameName;
-
-    public void searchView(String query) {
-
-    }
-    public void openFilters() {
-
-    }
-    public void openWishlists() {
-
-    }
-    public void openGameView(Game game) {
-
-    }
-}
+//public class MainView {
+//    private String search;
+//    private JPanel mainViewPanel;
+//    private JButton favoriteButton;
+//    private JLabel selectedGame;
+//    private JLabel gameName;
+//
+//    public void searchView(String query) {
+//
+//    }
+//    public void openFilters() {
+//
+//    }
+//    public void openWishlists() {
+//
+//    }
+//    public void openGameView(Game game) {
+//
+//    }
+//}
