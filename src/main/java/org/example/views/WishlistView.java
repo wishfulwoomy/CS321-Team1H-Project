14889620ff2ae1b38996848765701f9433331445
Eve Wall <emw0163@uah.edu>
2026-04-12 14:01:35 -0400

    Added method to WishlistView and created wishlistView.fxml

--- src/main/java/org/example/views/WishlistView.java
+++ src/main/java/org/example/views/WishlistView.java
@@ -1,4 +1,16 @@
 package main.java.org.example.views;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import main.java.org.example.model.Game;
+
 public class WishlistView {
+
+    @FXML
+
+    public Label selectWishlist;
 }
