61c0a1e8aa9d06c510448c78ee9163962609f15f
wishfulwoomy <filipookie@gmail.com>
2026-04-13 12:54:39 -0500

    Started working on mainView

--- src/main/java/org/example/views/MainView.java
+++ src/main/java/org/example/views/MainView.java
@@ -2,31 +2,48 @@ package main.java.org.example.views;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import main.java.org.example.model.Game;
 
 public class MainView implements Initializable {
+    private String search;
+    private Button favoriteButton;
+    private Label selectedGame;
+    private Label gameName;
+    /**
+     * Opens and closes the list of filters. Sits to the right of the search bar.
+     */
+    @FXML
+    private ToggleButton toggleFilters;
+    /**
+     * Container bar for all the individual filters.
+     * Starts invisible, toggled by toggleFilters.
+     */
+    @FXML
+    private ButtonBar filterBar;
 
     @FXML
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Put anything not done in scene builder here
     }
 
-    private String search;
-    private Button favoriteButton;
-    private Label selectedGame;
-    private Label gameName;
-
     public void searchView(String query) {
 
     }
-    public void openFilters() {
 
+    @FXML
+    public void openFilters(ActionEvent event) {
+        boolean isSelected = toggleFilters.isSelected();
+        filterBar.setVisible(isSelected);
+        filterBar.setManaged(isSelected);
     }
     public void openWishlists() {
 
