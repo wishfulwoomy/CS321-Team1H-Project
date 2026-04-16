18906edd5f6f6fe921f0d9774cd867272df87e1f
Eve Wall <emw0163@uah.edu>
2026-04-16 10:20:26 -0500

    Updated UserTest.java

--- src/test/java/UserTester.java
+++ src/test/java/UserTester.java
@@ -6,7 +6,7 @@ import main.java.org.example.model.Game;
 
 public class UserTester {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         User user1 = new User("Tater Tot", 1, "skatertot123");
 
@@ -25,13 +25,34 @@ public class UserTester {
         System.out.println("ID: " + user1.getID());
         System.out.println("Password: " + user1.getPassword());
 
-        
+
         Game game1 = new Game("Monopoly", 2, 8, 80);
         Game game2 = new Game("Uno", 2, 10, 120);
         Game game3 = new Game("Battleship", 2, 2, 60);
 
         Wishlist wlist1 = user1.createWishlist("friends");
+        wlist1.add(game1);
+        wlist1.add(game2);
+        wlist1.add(game3);
 
-        System.out.println(user1.getWishlists());
+
+        if (wlist1.getGames().contains(game1) && wlist1.getGames().contains(game2) && wlist1.getGames().contains(game3)) {
+            System.out.println("All 3 games added to wishlist");
+        } else {
+            System.out.println("Error! Games not added to the wishlist");
+        }
+
+        if (user1.getWishlists().contains(wlist1)) {
+            System.out.println("Wishlist added successfully");
+        } else {
+            System.out.println("Error! Wishlist not added");
+        }
+
+        user1.deleteWishlist(wlist1);
+        if (!user1.getWishlists().contains(wlist1)) {
+            System.out.println("Wishlist removed successfully");
+        } else {
+            System.out.println("Error! Wishlist not removed");
+        }
     }
 }
