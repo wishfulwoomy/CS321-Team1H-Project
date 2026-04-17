dcd23d7a092bb6269f4a9d07f2aaa38d05841e63
wishfulwoomy <filipookie@gmail.com>
2026-04-17 15:14:48 -0500

    Progress on gameView fxml and java

--- src/main/java/org/example/views/GameView.java
+++ src/main/java/org/example/views/GameView.java
@@ -5,23 +5,55 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import main.java.org.example.model.Game;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameView 
 {
+    // UI elements that display game info
     @FXML
-    private Hyperlink backToMainMenu;
+    private ImageView imageGamePicture;
+    @FXML
+    private Label labelGameTitle;
+    @FXML
+    private Text textAverageRating;
+    @FXML
+    private Text textDescription;
+    @FXML
+    private Text textReview;
+
+    // UI controls
+    @FXML
+    private Button buttonAddToList;
+    @FXML
+    private Button buttonLeaveReview;
+
+    // the game itself
+    private Game game;
 
     @FXML
     //@Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // put anything not done in scene builder here
+
+        labelGameTitle.setText(game.getTitle());
+        // imageGamePicture.setImage(game.); HAVE GAME CLASS RETURN IMAGE
+        textDescription.setText(game.getDescription());
+        // textReview.setText(game.) MAKE THIS A LOOP TO LOAD ALL REVIEWS
+    }
+
+    public void setGame(Game game) { // WORK ON LATER
+        this.game = game;
     }
 
     @FXML
